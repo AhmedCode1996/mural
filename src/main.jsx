79fafc1aa359299a2e31fc21d6c5fdeb09050467
AhmedCode1996 <ahmedharb960@gmail.com>
2023-10-05 14:06:28 +0300

    removing old style file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-import { GlobalStyle } from "./styles/globalStyles.js";
+
+import { GlobalStyle } from "./globalStyle.js";
 
 const root = document.getElementById("root");
 createRoot(root).render(
